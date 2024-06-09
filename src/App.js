ea323bfef33e136360da32c12c5f3cbf0a665ef0
MAhmadS <129769769+MAhmadS@users.noreply.github.com>
2024-06-09 17:29:17 +0500

    Added Component Logic in App component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
 import AdminNavbar from "./Components/AdminNavbar";
-import AdminCover from "./Components/AdminCover";
-import RegisterFranchise from "./Components/regFranchise";
 import FranchiseDetails from "./Components/FranchiseDetails";
+import Campaign from "./Components/Campaign";
+import Beneficiary from "./Components/Beneficiary";
+import ApproveProject from "./Components/ApproveProjects";
+import Projects from "./Components/Projects";
+import ListVolunteer from "./Components/volunteerList";
 import FranchiseLogin from "./Components/FranchiseLogin";
-import Franchises from "./Components/Franchise";
+import CompletedProjects from "./Components/CompletedProjects"; 
+import CompletedCampaigns from "./Components/CompletedCampaigns";
 
 const App = () => {
   const location = useLocation();
   return (
     <>
-      {location.pathname !== '/' && location.pathname !== '/managerlogin' && <AdminNavbar />}
+      {location.pathname !== '/' && <AdminNavbar />}
       <Routes>
-        <Route exact path="/" element={<AdminCover />} />
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        <Route exact path="/regFranchise" element={<RegisterFranchise />} />
-        <Route exact path="/franchise" element={<Franchises />} />
-
+        <Route exact path="/" element={<FranchiseLogin />} />
         <Route exact path="/franchiseInformation" element={<FranchiseDetails />} />
-        <Route exact path="/managerlogin" element={<FranchiseLogin />} />
-        
+        <Route exact path="/beneficiary" element={<Beneficiary />} />
       </Routes>
     </>
   );
